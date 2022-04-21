985d3600e030dac20e3f143cf89db2b91b55bb2e
Rongmario <rongmario@protonmail.com>
2022-04-21 12:29:16 +0100

    LoggedTimer

--- src/main/java/mezz/jei/util/LoggedTimer.java
+++ src/main/java/mezz/jei/util/LoggedTimer.java
@@ -1,2 +1,22 @@
-package mezz.jei.util;public class LoggedTimer {
+package mezz.jei.util;
+
+import com.google.common.base.Stopwatch;
+
+public class LoggedTimer {
+
+    private final Stopwatch stopWatch = Stopwatch.createUnstarted();
+    private String message = "";
+
+    public void start(String message) {
+        this.message = message;
+        Log.get().info("{}...", message);
+        stopWatch.reset();
+        stopWatch.start();
+    }
+
+    public void stop() {
+        stopWatch.stop();
+        Log.get().info("{} took {}", message, stopWatch);
+    }
+
 }
