a4070726fef7b567a9a916339b953cd725ed419f
Rongmario <rongmario@proton.me>
2025-11-18 15:08:44 +0000

    Only draw bookmark button and tooltip if list is displayed

--- src/main/java/mezz/jei/gui/overlay/bookmarks/BookmarkOverlay.java
+++ src/main/java/mezz/jei/gui/overlay/bookmarks/BookmarkOverlay.java
@@ -66,10 +66,10 @@ public class BookmarkOverlay implements ILeftAreaContent, IBookmarkOverlay {
 	public void drawScreen(Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
 		if (this.isListDisplayed()) {
 			this.contents.draw(minecraft, mouseX, mouseY, partialTicks);
+			if (!Config.hideBottomLeftCornerBookmarkButton()) {
+				this.bookmarkButton.draw(minecraft, mouseX, mouseY, partialTicks);
+			}
 		}
-        if (!Config.hideBottomLeftCornerBookmarkButton()) {
-            this.bookmarkButton.draw(minecraft, mouseX, mouseY, partialTicks);
-        }
 	}
 
 	@Override
@@ -80,10 +80,10 @@ public class BookmarkOverlay implements ILeftAreaContent, IBookmarkOverlay {
 	public void drawTooltips(Minecraft minecraft, int mouseX, int mouseY) {
 		if (isListDisplayed()) {
 			this.contents.drawTooltips(minecraft, mouseX, mouseY);
+			if (!Config.hideBottomLeftCornerBookmarkButton()) {
+				this.bookmarkButton.drawTooltips(minecraft, mouseX, mouseY);
+			}
 		}
-        if (!Config.hideBottomLeftCornerBookmarkButton()) {
-            bookmarkButton.drawTooltips(minecraft, mouseX, mouseY);
-        }
 	}
 
 	private static int getMinWidth() {
