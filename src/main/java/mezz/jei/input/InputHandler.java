246fc70b05fef9596d3310d96bec17f159c4b347
Rongmario <rongmario@proton.me>
2026-01-24 02:20:25 +0000

    Toggle overlay to work in GUIs and when text isn't focused, fixes #164

--- src/main/java/mezz/jei/input/InputHandler.java
+++ src/main/java/mezz/jei/input/InputHandler.java
@@ -289,6 +289,14 @@ public class InputHandler {
         }
 
         if (!isContainerTextFieldFocused()) {
+            if (KeyBindings.toggleOverlay.isActiveAndMatches(eventKey)) {
+                Config.toggleOverlayEnabled();
+                return true;
+            }
+            if (KeyBindings.toggleBookmarkOverlay.isActiveAndMatches(eventKey)) {
+                Config.toggleBookmarkEnabled();
+                return true;
+            }
             if (handleFocusKeybinds(eventKey)) {
                 return true;
             }
@@ -299,14 +307,6 @@ public class InputHandler {
     }
 
     private boolean handleGlobalKeybinds(int eventKey) {
-        if (KeyBindings.toggleOverlay.isActiveAndMatches(eventKey)) {
-            Config.toggleOverlayEnabled();
-            return false;
-        }
-        if (KeyBindings.toggleBookmarkOverlay.isActiveAndMatches(eventKey)) {
-            Config.toggleBookmarkEnabled();
-            return false;
-        }
         return ingredientListOverlay.onGlobalKeyPressed(eventKey);
     }
 
