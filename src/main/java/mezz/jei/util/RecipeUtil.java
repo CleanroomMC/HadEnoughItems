b2bb5c30d1ab79d78cc9e58fe08129adbc87e7cb
Rongmario <rongmario@proton.me>
2026-02-07 00:21:21 +0000

    Added input queries to RecipeUtil

--- src/main/java/mezz/jei/util/RecipeUtil.java
+++ src/main/java/mezz/jei/util/RecipeUtil.java
@@ -26,7 +26,7 @@ public final class RecipeUtil {
     /**
      * Gets all crafting recipes with HEI's internal recipes cache, with the matching output
      *
-     * @param output                        output ingredient of the crafting recipes
+     * @param  output                       output ingredient of the crafting recipes
      * @return                              crafting recipes that matches and has the output ingredient
      * @throws IllegalArgumentException     when the ingredient is not supported by HEI
      */
@@ -37,8 +37,8 @@ public final class RecipeUtil {
     /**
      * Gets all  recipes with HEI's internal recipes cache, with the matching output
      *
-     * @param output                        output ingredient of the crafting recipes
-     * @param predicate                     filter for what type of recipe wrapper to match
+     * @param  output                       output ingredient of the crafting recipes
+     * @param  predicate                    filter for what type of recipe wrapper to match
      * @return                              crafting recipes that matches and has the output ingredient
      * @throws IllegalArgumentException     when the ingredient is not supported by HEI
      */
@@ -49,13 +49,54 @@ public final class RecipeUtil {
     /**
      * Gets all recipes with HEI's internal recipes cache, with the matching output
      *
-     * @param output                        output ingredient of the recipes
+     * @param  output                       output ingredient of the recipes
      * @return                              recipes that matches and has the output ingredient
      * @throws IllegalArgumentException     when the ingredient is not supported by HEI
      */
     public static <V> List<IRecipeWrapper> getRecipesWithOutput(V output) throws IllegalArgumentException {
-        IIngredientHelper<V> helper = Internal.getIngredientRegistry().getIngredientHelper(output);
-        IFocus<?> focus = helper.translateFocus(new Focus<>(IFocus.Mode.OUTPUT, output), Focus::new);
+        return getRecipesWithFocus(output, IFocus.Mode.OUTPUT);
+    }
+
+    /**
+     * Gets all recipes with HEI's internal recipes cache, with the matching input
+     * Beware: this can be multiple times slower than {@link RecipeUtil#getRecipesWithOutput} variants
+     *
+     * @param  input                        input ingredient of the recipes
+     * @return                              crafting recipes that matches and has the input ingredient
+     * @throws IllegalArgumentException     when the ingredient is not supported by HEI
+     */
+    public static <V> List<IRecipeWrapper> getCraftingRecipesWithInput(V input) throws IllegalArgumentException {
+        return getRecipesWithInput(input, ICraftingRecipeWrapper.class::isInstance);
+    }
+
+    /**
+     * Gets all recipes with HEI's internal recipes cache, with the matching input
+     * Beware: this can be multiple times slower than {@link RecipeUtil#getRecipesWithOutput} variants
+     *
+     * @param  input                        input ingredient of the recipes
+     * @param  predicate                    filter for what type of recipe wrapper to match
+     * @return                              recipes that matches and has the input ingredient
+     * @throws IllegalArgumentException     when the ingredient is not supported by HEI
+     */
+    public static <V> List<IRecipeWrapper> getRecipesWithInput(V input, Predicate<IRecipeWrapper> predicate) throws IllegalArgumentException {
+        return getRecipesWithInput(input).stream().filter(predicate).collect(Collectors.toList());
+    }
+
+    /**
+     * Gets all recipes with HEI's internal recipes cache, with the matching input
+     * Beware: this can be multiple times slower than {@link RecipeUtil#getRecipesWithOutput} variants
+     *
+     * @param  input                        input ingredient of the recipes
+     * @return                              recipes that matches and has the input ingredient
+     * @throws IllegalArgumentException     when the ingredient is not supported by HEI
+     */
+    public static <V> List<IRecipeWrapper> getRecipesWithInput(V input) throws IllegalArgumentException {
+        return getRecipesWithFocus(input, IFocus.Mode.INPUT);
+    }
+
+    private static <V> List<IRecipeWrapper> getRecipesWithFocus(V ingredient, IFocus.Mode focusMode) throws IllegalArgumentException {
+        IIngredientHelper<V> helper = Internal.getIngredientRegistry().getIngredientHelper(ingredient);
+        IFocus<?> focus = helper.translateFocus(new Focus<>(focusMode, ingredient), Focus::new);
 
         IRecipeRegistry recipeRegistry = Internal.getRuntime().getRecipeRegistry();
         List<IRecipeCategory> recipeCategories = recipeRegistry.getRecipeCategories(focus);
