5d57719d91821e18f332ef56eb085ec5334206db
Kacper Bajenski <erednay29@gmail.com>
2026-02-19 10:01:58 +0100

    Add a hotkey for bookmarking recipes

--- src/main/java/mezz/jei/gui/recipes/RecipeBookmarkButton.java
+++ src/main/java/mezz/jei/gui/recipes/RecipeBookmarkButton.java
@@ -52,13 +52,7 @@ public class RecipeBookmarkButton extends GuiIconButtonSmall {
         if (!Config.isBookmarkOverlayEnabled()) {
             Config.toggleBookmarkEnabled();
         }
-        BookmarkList bookmarkList = Internal.getBookmarkList();
-        RecipeBookmarkGroup group = new RecipeBookmarkGroup(bookmarkList.nextId());
-        RecipeBookmarkItem<?> recipeBookmarkItem = new RecipeBookmarkItem<>(recipeLayout.getRecipeFavoriteButton().getDisplayedIngredient());
-        recipeBookmarkItem.setGroup(group); // Do this early so that the dummy items are also added.
-        recipeBookmarkItem.populateWith(recipe, category);
-        group.addItem(recipeBookmarkItem); // Do this late so that the recipe isn't overwritten.
-        group.update();
-        return bookmarkList.add(group);
+       
+        return recipeLayout.addToBookmarks();
     }
 }
