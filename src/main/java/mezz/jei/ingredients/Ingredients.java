0f65abe780926d37d9f418ed057e0e0c32020788
Rongmario <rongmario@proton.me>
2026-04-28 13:55:26 +0100

    Testing Testing 123

--- src/main/java/mezz/jei/ingredients/Ingredients.java
+++ src/main/java/mezz/jei/ingredients/Ingredients.java
@@ -3,18 +3,18 @@ package mezz.jei.ingredients;
 import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import mezz.jei.Internal;
 import mezz.jei.api.ingredients.IIngredientHelper;
-import mezz.jei.api.ingredients.IIngredientRegistry;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IIngredientType;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class Ingredients implements IIngredients {
-	private final Map<IIngredientType, List<List>> inputs = new Reference2ObjectOpenHashMap<>();
-	private final Map<IIngredientType, List<List>> outputs = new Reference2ObjectOpenHashMap<>();
+	private final Map<IIngredientType, List<Object>> inputs = new Reference2ObjectOpenHashMap<>();
+	private final Map<IIngredientType, List<Object>> outputs = new Reference2ObjectOpenHashMap<>();
 
 	@Override
 	public <T> void setInput(IIngredientType<T> ingredientType, T input) {
@@ -24,50 +24,41 @@ public class Ingredients implements IIngredients {
 	@Override
 	@Deprecated
 	public <T> void setInput(Class<? extends T> ingredientClass, T input) {
-		IngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		setInput(ingredientType, input);
-	}
-
-	@Override
-	public <T> void setInputLists(IIngredientType<T> ingredientType, List<List<T>> inputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientHelper<T> ingredientHelper = ingredientRegistry.getIngredientHelper(ingredientType);
-		List<List> expandedInputs = new ArrayList<>();
-		for (List<T> input : inputs) {
-			List<T> itemStacks = ingredientHelper.expandSubtypes(input);
-			expandedInputs.add(itemStacks);
-		}
-
-		this.inputs.put(ingredientType, expandedInputs);
-	}
-
-	@Override
-	@Deprecated
-	public <T> void setInputLists(Class<? extends T> ingredientClass, List<List<T>> inputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		setInputLists(ingredientType, inputs);
+		setInput(Internal.getIngredientRegistry().getIngredientType(ingredientClass), input);
 	}
 
 	@Override
 	public <T> void setInputs(IIngredientType<T> ingredientType, List<T> inputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientHelper<T> ingredientHelper = ingredientRegistry.getIngredientHelper(ingredientType);
-		List<List> expandedInputs = new ArrayList<>();
+		IIngredientHelper<T> ingredientHelper = Internal.getIngredientRegistry().getIngredientHelper(ingredientType);
+		List<Object> slots = new ArrayList<>(inputs.size());
 		for (T input : inputs) {
-			List<T> expandedInput = ingredientHelper.expandSubtypes(Collections.singletonList(input));
-			expandedInputs.add(expandedInput);
+			List<T> expanded = ingredientHelper.expandSubtypes(Collections.singletonList(input));
+			slots.add(expanded.size() == 1 ? expanded.get(0) : expanded);
 		}
-		this.inputs.put(ingredientType, expandedInputs);
+		this.inputs.put(ingredientType, slots);
 	}
 
 	@Override
 	@Deprecated
 	public <T> void setInputs(Class<? extends T> ingredientClass, List<T> input) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		setInputs(ingredientType, input);
+		setInputs(Internal.getIngredientRegistry().getIngredientType(ingredientClass), input);
+	}
+
+	@Override
+	public <T> void setInputLists(IIngredientType<T> ingredientType, List<List<T>> inputs) {
+		IIngredientHelper<T> ingredientHelper = Internal.getIngredientRegistry().getIngredientHelper(ingredientType);
+		List<Object> slots = new ArrayList<>(inputs.size());
+		for (List<T> input : inputs) {
+			List<T> expanded = ingredientHelper.expandSubtypes(input);
+			slots.add(expanded.size() == 1 ? expanded.get(0) : expanded);
+		}
+		this.inputs.put(ingredientType, slots);
+	}
+
+	@Override
+	@Deprecated
+	public <T> void setInputLists(Class<? extends T> ingredientClass, List<List<T>> inputs) {
+		setInputLists(Internal.getIngredientRegistry().getIngredientType(ingredientClass), inputs);
 	}
 
 	@Override
@@ -78,110 +69,112 @@ public class Ingredients implements IIngredients {
 	@Override
 	@Deprecated
 	public <T> void setOutput(Class<? extends T> ingredientClass, T output) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		setOutput(ingredientType, output);
+		setOutput(Internal.getIngredientRegistry().getIngredientType(ingredientClass), output);
 	}
 
 	@Override
 	public <T> void setOutputs(IIngredientType<T> ingredientType, List<T> outputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientHelper<T> ingredientHelper = ingredientRegistry.getIngredientHelper(ingredientType);
-		List<List> expandedOutputs = new ArrayList<>();
+		IIngredientHelper<T> ingredientHelper = Internal.getIngredientRegistry().getIngredientHelper(ingredientType);
+		List<Object> slots = new ArrayList<>(outputs.size());
 		for (T output : outputs) {
-			List<T> expandedOutput = ingredientHelper.expandSubtypes(Collections.singletonList(output));
-			expandedOutputs.add(expandedOutput);
+			List<T> expanded = ingredientHelper.expandSubtypes(Collections.singletonList(output));
+			slots.add(expanded.size() == 1 ? expanded.get(0) : expanded);
 		}
-
-		this.outputs.put(ingredientType, expandedOutputs);
+		this.outputs.put(ingredientType, slots);
 	}
 
 	@Override
 	@Deprecated
 	public <T> void setOutputs(Class<? extends T> ingredientClass, List<T> outputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		setOutputs(ingredientType, outputs);
+		setOutputs(Internal.getIngredientRegistry().getIngredientType(ingredientClass), outputs);
 	}
 
 	@Override
 	public <T> void setOutputLists(IIngredientType<T> ingredientType, List<List<T>> outputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientHelper<T> ingredientHelper = ingredientRegistry.getIngredientHelper(ingredientType);
-		List<List> expandedOutputs = new ArrayList<>();
+		IIngredientHelper<T> ingredientHelper = Internal.getIngredientRegistry().getIngredientHelper(ingredientType);
+		List<Object> slots = new ArrayList<>(outputs.size());
 		for (List<T> output : outputs) {
-			List<T> itemStacks = ingredientHelper.expandSubtypes(output);
-			expandedOutputs.add(itemStacks);
+			List<T> expanded = ingredientHelper.expandSubtypes(output);
+			slots.add(expanded.size() == 1 ? expanded.get(0) : expanded);
 		}
-
-		this.outputs.put(ingredientType, expandedOutputs);
+		this.outputs.put(ingredientType, slots);
 	}
 
 	@Override
 	@Deprecated
 	public <T> void setOutputLists(Class<? extends T> ingredientClass, List<List<T>> outputs) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		setOutputLists(ingredientType, outputs);
+		setOutputLists(Internal.getIngredientRegistry().getIngredientType(ingredientClass), outputs);
 	}
 
 	@Override
 	public <T> List<List<T>> getInputs(IIngredientType<T> ingredientType) {
-		@SuppressWarnings("unchecked")
-		List<List<T>> inputs = (List<List<T>>) (Object) this.inputs.get(ingredientType);
-		if (inputs == null) {
-			return Collections.emptyList();
-		}
-		return inputs;
+		List<Object> slots = this.inputs.get(ingredientType);
+		return slots == null ? Collections.emptyList() : new SlotView<>(slots);
 	}
 
 	@Override
 	@Deprecated
 	public <T> List<List<T>> getInputs(Class<? extends T> ingredientClass) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		return getInputs(ingredientType);
+		return getInputs(Internal.getIngredientRegistry().getIngredientType(ingredientClass));
 	}
 
 	@Override
 	public <T> List<List<T>> getOutputs(IIngredientType<T> ingredientType) {
-		@SuppressWarnings("unchecked")
-		List<List<T>> outputs = (List<List<T>>) (Object) this.outputs.get(ingredientType);
-		if (outputs == null) {
-			return Collections.emptyList();
-		}
-		return outputs;
+		List<Object> slots = this.outputs.get(ingredientType);
+		return slots == null ? Collections.emptyList() : new SlotView<>(slots);
 	}
 
 	@Override
 	@Deprecated
 	public <T> List<List<T>> getOutputs(Class<? extends T> ingredientClass) {
-		IIngredientRegistry ingredientRegistry = Internal.getIngredientRegistry();
-		IIngredientType<T> ingredientType = ingredientRegistry.getIngredientType(ingredientClass);
-		return getOutputs(ingredientType);
+		return getOutputs(Internal.getIngredientRegistry().getIngredientType(ingredientClass));
 	}
 
 	public Map<IIngredientType, List> getInputIngredients() {
-		Map<IIngredientType, List> inputIngredients = new Reference2ObjectOpenHashMap<>();
-		for (Map.Entry<IIngredientType, List<List>> entry : inputs.entrySet()) {
-			List<Object> flatIngredients = new ArrayList<>();
-			for (List ingredients : entry.getValue()) {
-				flatIngredients.addAll(ingredients);
-			}
-			inputIngredients.put(entry.getKey(), flatIngredients);
+		Map<IIngredientType, List> result = new Reference2ObjectOpenHashMap<>();
+		for (Map.Entry<IIngredientType, List<Object>> entry : inputs.entrySet()) {
+			result.put(entry.getKey(), flatten(entry.getValue()));
 		}
-		return inputIngredients;
+		return result;
 	}
 
 	public Map<IIngredientType, List> getOutputIngredients() {
-		Map<IIngredientType, List> outputIngredients = new Reference2ObjectOpenHashMap<>();
-		for (Map.Entry<IIngredientType, List<List>> entry : outputs.entrySet()) {
-			List<Object> flatIngredients = new ArrayList<>();
-			for (List ingredients : entry.getValue()) {
-				flatIngredients.addAll(ingredients);
-			}
-			outputIngredients.put(entry.getKey(), flatIngredients);
+		Map<IIngredientType, List> result = new Reference2ObjectOpenHashMap<>();
+		for (Map.Entry<IIngredientType, List<Object>> entry : outputs.entrySet()) {
+			result.put(entry.getKey(), flatten(entry.getValue()));
 		}
-		return outputIngredients;
+		return result;
+	}
+
+	private static List<Object> flatten(List<Object> slots) {
+		List<Object> flat = new ArrayList<>(slots.size());
+		for (Object slot : slots) {
+			if (slot instanceof List) {
+				flat.addAll((List<?>) slot);
+			} else {
+				flat.add(slot);
+			}
+		}
+		return flat;
+	}
+
+	private static class SlotView<T> extends AbstractList<List<T>> {
+		private final List<Object> slots;
+
+		SlotView(List<Object> slots) {
+			this.slots = slots;
+		}
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public List<T> get(int index) {
+			Object slot = slots.get(index);
+			return slot instanceof List ? (List<T>) slot : Collections.singletonList((T) slot);
+		}
+
+		@Override
+		public int size() {
+			return slots.size();
+		}
 	}
 }
