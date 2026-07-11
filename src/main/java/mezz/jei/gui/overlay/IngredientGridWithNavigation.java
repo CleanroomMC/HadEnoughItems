49d1d490ff9eaa6b8d8b3d786c4b0d35114df2cf
mezz <tehgeek@gmail.com>
2026-07-11 10:02:41 +0800

    Prevent ingredient grids from consuming scroll events when there is only 1 page
    (cherry picked from commit 740ca57c0b02142ba5fba9a1091d41c8a1853121)

--- src/main/java/mezz/jei/gui/overlay/IngredientGridWithNavigation.java
+++ src/main/java/mezz/jei/gui/overlay/IngredientGridWithNavigation.java
@@ -145,22 +145,18 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IMouse
 	@Override
 	public boolean handleMouseScrolled(int mouseX, int mouseY, int scrollDelta) {
 		if (scrollDelta < 0) {
-			this.pageDelegate.nextPage();
-			return true;
+			return this.pageDelegate.nextPage();
 		} else if (scrollDelta > 0) {
-			this.pageDelegate.previousPage();
-			return true;
+			return this.pageDelegate.previousPage();
 		}
 		return false;
 	}
 
 	public boolean onKeyPressed(char typedChar, int keyCode) {
 		if (KeyBindings.nextPage.isActiveAndMatches(keyCode)) {
-			this.pageDelegate.nextPage();
-			return true;
+			return this.pageDelegate.nextPage();
 		} else if (KeyBindings.previousPage.isActiveAndMatches(keyCode)) {
-			this.pageDelegate.previousPage();
-			return true;
+			return this.pageDelegate.previousPage();
 		}
 		return checkHotbarKeys(keyCode);
 	}
@@ -225,6 +221,9 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IMouse
 	private class IngredientGridPaged implements IPaged {
 		@Override
 		public boolean nextPage() {
+			if (getPageCount() <= 1) {
+				return false;
+			}
 			final int itemsCount = ingredientSource.collapsedSize();
 			if (itemsCount > 0) {
 				firstItemIndex += ingredientGrid.size();
@@ -242,6 +241,9 @@ public class IngredientGridWithNavigation implements IShowsRecipeFocuses, IMouse
 
 		@Override
 		public boolean previousPage() {
+			if (getPageCount() <= 1) {
+				return false;
+			}
 			final int itemsPerPage = ingredientGrid.size();
 			if (itemsPerPage == 0) {
 				firstItemIndex = 0;
