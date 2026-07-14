8fa5baec5977895348d2530fbb1bf28f8c586f1a
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-07-14 16:14:31 +0800

    skip scroll handler if mouse is not over it

--- src/main/java/mezz/jei/input/InputHandler.java
+++ src/main/java/mezz/jei/input/InputHandler.java
@@ -157,7 +157,15 @@ public class InputHandler {
 	}
 
 	private boolean handleMouseScroll(int dWheel, int mouseX, int mouseY) {
-		return ingredientListOverlay.handleMouseScrolled(mouseX, mouseY, dWheel) || leftAreaDispatcher.handleMouseScrolled(mouseX, mouseY, dWheel);
+        if (ingredientListOverlay.isMouseOver(mouseX, mouseY) && ingredientListOverlay.handleMouseScrolled(mouseX, mouseY, dWheel)) {
+            return true;
+        }
+
+        if (leftAreaDispatcher.isMouseOver(mouseX, mouseY) && leftAreaDispatcher.handleMouseScrolled(mouseX, mouseY, dWheel)) {
+            return true;
+        }
+
+        return false;
 	}
 
 	private boolean handleMouseClick(GuiScreen guiScreen, int mouseButton, int mouseX, int mouseY) {
