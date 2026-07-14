cdeb1b6bf213fd16d13743826dc1f81b8f467fc3
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-07-14 16:14:11 +0800

    add exclusion area check to `IngredientListOverlay`

--- src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
+++ src/main/java/mezz/jei/gui/overlay/IngredientListOverlay.java
@@ -220,6 +220,9 @@ public class IngredientListOverlay implements IIngredientListOverlay, IMouseHand
 
 	@Override
 	public boolean isMouseOver(int mouseX, int mouseY) {
+		if (guiScreenHelper.isInGuiExclusionArea(mouseX, mouseY)) {
+			return false;
+		}
 		if (isListDisplayed()) {
 			if (Config.isCenterSearchBarEnabled() && searchField.isMouseOver(mouseX, mouseY)) {
 				return true;
