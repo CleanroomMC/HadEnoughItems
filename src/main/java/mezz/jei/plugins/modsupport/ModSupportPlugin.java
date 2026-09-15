c00958feb27b11fa1ebaff354239151ba246dfbf
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-09-16 00:46:45 +0800

    add autocrafting support for: fastbench, tconstruct, projecte

--- src/main/java/mezz/jei/plugins/modsupport/ModSupportPlugin.java
+++ src/main/java/mezz/jei/plugins/modsupport/ModSupportPlugin.java
@@ -1,10 +1,16 @@
 package mezz.jei.plugins.modsupport;
 
 import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
 import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import mezz.jei.api.recipe.transfer.IRecipeTransferRegistry;
+import mezz.jei.util.Log;
+import net.minecraft.inventory.Container;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 @JEIPlugin
@@ -22,4 +28,44 @@ public class ModSupportPlugin implements IModPlugin {
         return ForgeRegistries.ITEMS.getValue(new ResourceLocation(namespace, path));
     }
 
+    @Override
+    public void register(IModRegistry registry) {
+        IRecipeTransferRegistry recipeTransferRegistry = registry.getRecipeTransferRegistry();
+
+        Class<? extends Container> toAttachOutput;
+
+        toAttachOutput = findContainerClass("fastbench", "shadows.fastbench.gui.ContainerFastBench");
+        if (toAttachOutput != null) {
+            recipeTransferRegistry.attachOutputSlotProvider(toAttachOutput, VanillaRecipeCategoryUid.CRAFTING, 0);
+        }
+
+        toAttachOutput = findContainerClass("fastbench", "shadows.fastbench.gui.ClientContainerFastBench");
+        if (toAttachOutput != null) {
+            recipeTransferRegistry.attachOutputSlotProvider(toAttachOutput, VanillaRecipeCategoryUid.CRAFTING, 0);
+        }
+
+        toAttachOutput = findContainerClass("tconstruct", "slimeknights.tconstruct.tools.common.inventory.ContainerCraftingStation");
+        if (toAttachOutput != null) {
+            recipeTransferRegistry.attachOutputSlotProvider(toAttachOutput, VanillaRecipeCategoryUid.CRAFTING, 0);
+        }
+
+        toAttachOutput = findContainerClass("projecte", "moze_intel.projecte.gameObjs.container.PhilosStoneContainer");
+        if (toAttachOutput != null) {
+            recipeTransferRegistry.attachOutputSlotProvider(toAttachOutput, VanillaRecipeCategoryUid.CRAFTING, 0);
+        }
+    }
+
+    private Class<? extends Container> findContainerClass(String modId, String className) {
+        if (!Loader.isModLoaded(modId)) {
+            return null;
+        }
+        try {
+            @SuppressWarnings("unchecked")
+            Class<? extends Container> clazz = (Class<? extends Container>) Class.forName(className);
+            return clazz;
+        } catch (Exception e) {
+            Log.get().error("Found '{}' mod but unable to find class '{}'", modId, className, e);
+            return null;
+        }
+    }
 }
