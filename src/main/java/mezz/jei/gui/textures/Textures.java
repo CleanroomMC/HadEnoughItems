8506b45cd7658eaac5b213822061a3dc6c0ace0f
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-09-17 20:28:40 +0800

    Fix texture trim handling (#247)
    * remove trim for arrow icon, trimming breaks arrow shadow

--- src/main/java/mezz/jei/gui/textures/Textures.java
+++ src/main/java/mezz/jei/gui/textures/Textures.java
@@ -58,10 +58,8 @@ public class Textures {
 
 		this.shapelessIcon = registerGuiSprite("icons/shapeless_icon", 36, 36)
 			.trim(1, 2, 1, 1);
-		this.arrowPrevious = registerGuiSprite("icons/arrow_previous", 9, 9)
-			.trim(0, 0, 1, 1);
-		this.arrowNext = registerGuiSprite("icons/arrow_next", 9, 9)
-			.trim(0, 0, 1, 1);
+		this.arrowPrevious = registerGuiSprite("icons/arrow_previous", 9, 9);
+		this.arrowNext = registerGuiSprite("icons/arrow_next", 9, 9);
 		this.recipeTransfer = registerGuiSprite("icons/recipe_transfer", 7, 7);
 		this.favoriteDisabled = registerGuiSprite("icons/favorite_disabled", 7, 7);
 		this.favoriteEnabled = registerGuiSprite("icons/favorite_enabled", 7, 7);
